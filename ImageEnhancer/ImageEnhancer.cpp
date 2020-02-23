bfbec90caacd4bac3f946837e9b06f4555267bf4
joepearson95 <joe.c.pearson95@gmail.com>
2020-02-23 18:16:38 +0000

    First commit

---
+++ ImageEnhancer/ImageEnhancer.cpp
@@ -0,0 +1,36 @@
+#define CL_USE_DEPRECATED_OPENCL_1_2_APIS
+#include <CL/cl.hpp>
+#include <fstream>
+#include <iostream>
+
+int main() {
+	std::vector<cl::Platform> platforms;
+	cl::Platform::get(&platforms);
+
+	auto platform = platforms.front();
+	std::vector<cl::Device> devices;
+	platform.getDevices(CL_DEVICE_TYPE_GPU, &devices);
+
+	auto device = devices.front();
+
+	std::ifstream helloworldFile("mykernel.cl");
+	std::string src(std::istreambuf_iterator<char> (helloworldFile), (std::istreambuf_iterator<char>()));
+
+	cl::Program::Sources sources(1, std::make_pair(src.c_str(), src.length() + 1));
+
+	cl::Context context(device);
+	cl::Program program(context, sources);
+
+	auto err = program.build("-cl-std=CL1.2");
+
+	char buff[16];
+	cl::Buffer memBuff(context, CL_MEM_WRITE_ONLY | CL_MEM_HOST_READ_ONLY, sizeof(buff));
+	cl::Kernel kernel(program, "mykernel", &err);
+	kernel.setArg(0, memBuff);
+
+	cl::CommandQueue queue(context, device);
+	queue.enqueueTask(kernel);
+	queue.enqueueReadBuffer(memBuff, CL_TRUE, 0, sizeof(buff), buff);
+
+	std::cout << buff;
+}
